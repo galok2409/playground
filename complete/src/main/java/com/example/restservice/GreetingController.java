b9999f99b7ca0461115ca8fb71992a79ee4beffb
Alok Gupta <alok.gupta@cellpointmobile.com>
2024-07-02 17:53:18 +0530

    message change

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World GKE!@") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World GKE!$") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
